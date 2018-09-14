eb79909c9679182fa4da3f321def98adb3f12ff8
20171105111 <2604406667@qq.com>
2018-09-14 11:06:11 +0800

    Dict

---
+++ main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  L06Dict
+//
+//  Created by 徐涛 on 2018/9/14.
+//  Copyright © 2018年 徐涛. All rights reserved.
+//
+
+import Foundation
+var dict = ["name":"sfddsfds","age":"16"]
+dict["sex"] = "felame"
+print(dict)
+print(dict["name"])
+
